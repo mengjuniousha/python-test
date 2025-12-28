dbad83b767684738f8b98e330ca99aae67017820
mengjuniousha <wuwuudm4693@outlook.com>
2025-12-29 05:08:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-# Last updated: 2025-12-26 04:05:35
+# Last updated: 2025-12-29 05:08:17
 
 class Calculator:    def add(self, a, b):        return a + b        def multiply(self, a, b):        return a * bif __name__ == "__main__":    calc = Calculator()    print(calc.add(5, 3))
